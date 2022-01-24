d85cc27b79bb6908c6a6cc856f9f5ccf2af4a046
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2022-01-24 17:13:56 +0700

    add documentation

--- data-generator/generate-kota.py
+++ data-generator/generate-kota.py
@@ -1,15 +1,26 @@
 import csv
 import config
 
+# buka file yang akan ditulis dengan mode write (akan melakukan overwrite)
 f = open('csv/kota.csv', 'w')
+
+# inisiasi `writer` dari csv untuk dapat menulis kedalam file csv
 writer = csv.writer(f)
 
+# header yang akan ditulis (berupa list)
 header = ['id', 'nama']
+
+# tulis header ke file csv
 writer.writerow(header)
 
-id = 1
+id = 0
+
+# looping untuk menulis data ke file csv
 for _kota in config.kota:
     id = id + 1
+
+    # tulis baris ke file csv
     writer.writerow([id,_kota])
 
+# tutup file agar tidak terjadi error
 f.close()
--- data-generator/generate-soal.py
+++ data-generator/generate-soal.py
@@ -3,16 +3,16 @@ import csv
 import config
 import json
 
-# inisiasi objek Faker
+# inisiasi object Faker
 fake = Faker()
 
+# ambil total soal dari config
 total_soal = config.total_soal
 id = 0
 
 f = open("csv/soal.csv", "w")
 writer = csv.writer(f)
 header = ["id", "id_mapel", "body", "pilihan_jawaban", "jawaban_benar"]
-
 writer.writerow(header)
 
 total_mapel = len(config.mata_pelajaran)
@@ -20,14 +20,18 @@ for idx_mapel in range(total_mapel):
     id_mapel = idx_mapel + 1
     for i in range(total_soal):
         id += 1
+
+        # gunakan faker untuk membuat susunan string random maksimal 200 karakter
         body = fake.text(max_nb_chars=200)
+
         pilihan_jawaban = dict()
         for pilihan in config.pilihan_jawaban:
             pilihan_jawaban[pilihan] = json.dumps(fake.text(max_nb_chars=200))
 
-        # pilih satu jawaban random
+        # pilih satu jawaban random berdasarkan list dari config
         jawaban_benar = fake.random_element(elements=config.pilihan_jawaban)
-        # ubah dict menjadi json
+
+        # ubah dict menjadi json agar mudah disimpan di database
         pilihan_jawaban_json = json.dumps(pilihan_jawaban)
 
         writer.writerow([id, id_mapel, body, pilihan_jawaban_json, jawaban_benar])
