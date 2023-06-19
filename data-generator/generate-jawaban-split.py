f78f8f37199450c9088fd5432f319aef5ad8f391
DidaPrasetyo <didaprasetyorahmat@gmail.com>
2023-06-20 04:20:09 +0700

    fix problem in generate-jawaban-split (final)

--- data-generator/generate-jawaban-split.py
+++ data-generator/generate-jawaban-split.py
@@ -18,6 +18,7 @@ def create_file():
 
     # f = open("csv/jawaban.csv", "w")
     f = open(f"csv/jawaban-{file_number}.csv", "w")
+    print("file_number: ", file_number)
     writer = csv.writer(f, doublequote=True, quoting=csv.QUOTE_ALL, lineterminator="\n")
     header = ["id", "id_siswa", "id_soal", "id_mapel", "jawaban"]
     writer.writerow(header)
@@ -28,15 +29,6 @@ def create_file():
 for idx_siswa in range(total_siswa):
     id_siswa = idx_siswa + 1
 
-    # if id_siswa % config.total_siswa == 0 or id_siswa >= total_siswa - config.total_siswa:
-        
-    if id_siswa % 12500 == 0:
-        print("id siswa processed: ", id_siswa)
-        f.close()
-        [writer, f] = create_file()
-        print("file_number: ", file_number)
-        file_number += 1
-
     for idx_mapel in range(len(config.mata_pelajaran)) :
         id_mapel = idx_mapel+1
         for idx_soal in range(total_soal):
@@ -44,3 +36,9 @@ for idx_siswa in range(total_siswa):
             id_soal = idx_soal + 1
             jawaban = fake.random_element(elements=config.pilihan_jawaban)
             writer.writerow([id, id_siswa, id_soal, id_mapel, jawaban])
+        
+    if id_siswa % 12500 == 0:
+        print("id siswa processed: ", id_siswa)
+        f.close()
+        file_number += 1
+        [writer, f] = create_file()
